5a42c38b55a1f7589b28af712132b05ff2040983
David Schneider <davidschneider@Davids-MacBook-Pro.local>
2019-11-14 16:16:26 -0500

    adds phonebook application

---
+++ phone_book_app.py
@@ -0,0 +1,65 @@
+
+phonebook = {'David Schneider': '813-504-1955'}
+# display a menu
+def menu():
+    menu = """
+
+Electronic Phone Book
+=====================
+1. Look up an entry
+2. Set an entry
+3. Delete an entry
+4. List all entries
+5. Quit
+
+"""
+    print(menu)
+    user_input = input("What do you want to do (1-5)? ")
+    return user_input
+    
+# look up entry and display name/number
+def look_up_entry(name):
+    entry_phone = phonebook[name]
+    return entry_phone
+
+# add new name and number
+def set_entry(name, phone_number):
+    phonebook[name] = phone_number
+    return None
+
+# delete an entry
+def delete_entry(name):
+    del phonebook[name]
+    return None
+
+# print all entries in phone book
+def list_all_entries():
+    for name in phonebook:
+        print(f"{name}: {phonebook[name]}")
+    
+# quit program
+
+def main():
+
+    is_running = True
+    while is_running:
+        user_input = menu()
+        print(user_input)
+        if user_input == "1":
+            name_input = input("Enter name for lookup: ")
+            lookup_result = look_up_entry(name_input)
+            print(lookup_result)
+        elif user_input == "2":
+            entry_name = input("Enter name: ")
+            entry_number = input("Enter phonenumber: ")
+            new_entry = set_entry(entry_name, entry_number)
+            print(f"Entry stored for {entry_name}")
+        elif user_input == "3":
+            entry_name = input("Enter name: ")
+            delete_entry(entry_name)
+            print(f"Entry deleted for {entry_name}")
+        elif user_input == "4":
+        elif user_input == "5":
+            pass
+        else:
+main()
