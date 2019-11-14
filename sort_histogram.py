babf222c4017164b78c7c0b014fa4b7dae5ad096
David Schneider <davidschneider@Davids-MacBook-Pro.local>
2019-11-14 16:37:31 -0500

    adds new sorted histogram

--- sort_histogram.py
+++ sort_histogram.py
@@ -21,7 +21,7 @@ def word_histogram(user_sentence):
 result_histogram = word_histogram(user_sentence)
 # print(result_histogram)
 
-sorted_histogram = sorted(result_histogram.items(), key = lambda x: x[1], reverse=True)
+sorted_histogram = sorted(result_histogram.items(), key=lambda value: value[1], reverse=True)
 # sorted(result_histogram.values(), reverse=True))
 first_rank = sorted_histogram[0]
 second_rank = sorted_histogram[1]
